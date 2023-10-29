c5f260df2e646119cc2f979ef2d98adb53df5c4a
ilovetiktiok <alexdubovyck@yandex.ru>
2023-10-30 02:47:48 +0300

    Вариант 3 - ещё лучше

--- toMp4.bat
+++ toMp4.bat
@@ -4,11 +4,25 @@
 
 
 @echo off
+@REM setlocal enabledelayedexpansion
+:: Получить текущий диск, на котором находится скрипт
+:: for %%i in ("%~dp0") do set currentDrive=%%~di
+:: Запустить скрипт на текущем диске
+:: cmd /k "cd /d !currentDrive!\PythonProjects\toMP3\venv\Scripts & activate & cd /d !currentDrive!\PythonProjects\toMP3 & python toMp4Main.py"
+@REM pause
+
+
+
+@echo off
 setlocal enabledelayedexpansion
 
 :: Получить текущий диск, на котором находится скрипт
 for %%i in ("%~dp0") do set currentDrive=%%~di
 
+:: Собрать путь к venv и toMp4Main.py
+set "venvPath=!currentDrive!\!~dp0venv\Scripts"
+set "mainScriptPath=!currentDrive!\!~dp0toMp4Main.py"
+
 :: Запустить скрипт на текущем диске
-cmd /k "cd /d !currentDrive!\PythonProjects\toMP3\venv\Scripts & activate & cd /d !currentDrive!\PythonProjects\toMP3 & python toMp4Main.py"
+cmd /k "cd /d !venvPath! & activate & cd /d !mainScriptPath!"
 @REM pause
