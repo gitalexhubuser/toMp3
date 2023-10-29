7b7f627a7cc9f6b6ce6966c2aae8fe003fbcc1fe
ilovetiktiok <alexdubovyck@yandex.ru>
2023-10-30 02:23:18 +0300

    без указания диска

--- toMp4.bat
+++ toMp4.bat
@@ -1,2 +1,14 @@
-cmd /k "cd /d E:\PythonProjects\toMP3\venv\Scripts & activate & cd /d E:\PythonProjects\toMP3 & python toMp4Main.py"
+@REM cmd /k "cd /d E:\PythonProjects\toMP3\venv\Scripts & activate & cd /d E:\PythonProjects\toMP3 & python toMp4Main.py"
+@REM cmd /k "cd /d O:\PythonProjects\toMP3\venv\Scripts & activate & cd /d O:\PythonProjects\toMP3 & python toMp4Main.py"
+@REM pause
+
+
+@echo off
+setlocal enabledelayedexpansion
+
+:: Получить текущий диск, на котором находится скрипт
+for %%i in ("%~dp0") do set currentDrive=%%~di
+
+:: Запустить скрипт на текущем диске
+cmd /k "cd /d !currentDrive!\PythonProjects\toMP3\venv\Scripts & activate & cd /d !currentDrive!\PythonProjects\toMP3 & python toMp4Main.py"
 @REM pause
